835209b9db782a57250358a4e20afcbb92f848e4
jopera1516daw2 <jopera1516daw2@gmail.com>
2015-12-15 15:48:19 +0100

    Prova d'IC amb Jenkins

--- src/jenkins/Jenkins.java
+++ src/jenkins/Jenkins.java
@@ -17,6 +17,7 @@ public class Jenkins {
             bw.write("  </head>");bw.newLine();
             bw.write("  <body>");bw.newLine();
             bw.write("    Projecte per provar l'IC Jenkins");bw.newLine();
+            bw.write("    Modificació del projecte jenkins");bw.newLine();
             bw.write("  </body>");bw.newLine();
             bw.write("</html>");bw.newLine();
             bw.close();
